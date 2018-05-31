afe3a746921f106295e639f2bca816eae6f59b67
debjohnson <debjohnson_11@yahoo.com>
2018-05-31 18:20:03 -0500

    Added product component/imported into app

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Product from '../product/product';
+
 import HttpService from '../services/http-services';
 
 const http = new HttpService();
