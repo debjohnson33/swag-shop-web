5b3f5699064448dc984912e9c562e214d2e3c2f0
debjohnson <debjohnson_11@yahoo.com>
2018-05-31 21:35:15 -0500

    Connected notification service to wishList

--- src/services/notification-service.js
+++ src/services/notification-service.js
@@ -1,3 +1,4 @@
+export const NOTIF_WISHLIST_CHANGED = "notif_wishlist_changed";
 var observers = {}; 
 let instance = null;
 
@@ -7,6 +8,15 @@ class NotificationService {
 			instance = this;
 		}
 		return instance;
+	}
+
+	postNotification = (notifName, data) => {
+		let obs = observers[notifName];
+
+		for (var x = 0; x < obs.length; x++) {
+			var obj = obs[x];
+			obj.callBack(data);
+		}
 	}
 
 	removeObserver = (observer, notifName) => {
@@ -34,3 +44,5 @@ class NotificationService {
 		observers[notifName].push(obj);
 	}
 }
+
+export default NotificationService;
--- src/wishlist/wishlist.js
+++ src/wishlist/wishlist.js
@@ -1,34 +1,33 @@
 import React, { Component } from 'react';
 import './wishlist.css';
 import DataService from '../services/data-service';
-import NotificationService from '../services/notification-service';
+import NotificationService, {NOTIF_WISHLIST_CHANGED} from '../services/notification-service';
 
 import ProductCondensed from '../product-condensed/product-condensed';
+
+let ns = new NotificationService();
 
 class WishList extends Component {
 
 	constructor(props) {
 		super(props);
 
-		this.state = {wishList:[
-			{
-				title:"Bologna Killer",
-				price:23.99,
-				_id:"sdfsgjlj369"
-			},
-			{
-				title:"Foo Man Chu",
-				price:4.54,
-				_id:"s3edc7ujtnghb"
-			},
-			{
-				title:"Pipe Dream",
-				price:100,
-				_id:"9ol3yhij4rf09"
-			}
-		]}
+		this.state = {wishList:[]}
 
 		this.createWishList = this.createWishList.bind(this);
+		this.onWishListChanged = this.onWishListChanged.bind(this);
+	}
+
+	componentDidMount() {
+		ns.addObserver(NOTIF_WISHLIST_CHANGED, this, this.onWishListChanged);
+	}
+
+	componentWillUnMount() {
+		ns.removeObserver(this, NOTIF_WISHLIST_CHANGED)
+	}
+
+	onWishListChanged(newWishList) {
+		this.setState({wishList: newWishList})
 	}
 
 	createWishList = () => {
